e58a1e5598d03fedeebbc1cff685986aa615313b
Benjamin <benjamin.castromadrid28et32@gmail.com>
2025-10-12 18:24:37 -0300

    Ejercicio 5 + 6 terminados

--- guiaEjercicios2/src/main/java/ejercicio6/OficinaDeAlumnos.java
+++ guiaEjercicios2/src/main/java/ejercicio6/OficinaDeAlumnos.java
@@ -1,51 +1,19 @@
 package ejercicio6;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class OficinaDeAlumnos implements Runnable {
-	
-	public static List<Alumnos> listaAlumnos = Collections.synchronizedList(new ArrayList<>());
-	private String nombre, apellido;
-	
-	public OficinaDeAlumnos(String nombre, String apellido) {
-		this.nombre = nombre;
-		this.apellido = apellido;
-		}
-	
-	private static int getRandom(int min, int max) {
-		int range = (max - min) + 1;
-     		int random = (int) ((range * Math.random()) + min);
-		return random;
-		}
-	
-	private void OficinaDeAlumno(List<Alumnos> asistencia) {}
-	
-	private int[] Assist(int limit) {
-		int[] facts = new int[9];
-		for(int i = 0; i < limit; i++) {
-			int faltas = OficinaDeAlumnos.getRandom(1, 20);
-			facts[i] = faltas;
-			};
-			return facts;
-			}
-	
-	private int[] Grade(int limit) {
-		int[] notas = new int[3];
-		for(int i = 0; i < limit; i++) {
-			int faltas = OficinaDeAlumnos.getRandom(1, 10);
-			notas[i] = faltas;
-			};
-			return notas;
-			}
-	
-	@Override
-	public void run() {
-		boolean esAlumnoR = true;
-		Alumnos alumno = new Alumnos(nombre, apellido, Assist(9), Grade(3), esAlumnoR);
-		listaAlumnos.add(alumno);
-		OficinaDeAlumno(listaAlumnos);
-		System.out.println(alumno);
-		}
-	}
+
+    public static List<Alumnos> listaAlumnos = new ArrayList<>();
+
+    @Override
+    public void run() {
+        listaAlumnos.add(new Alumnos("Benjamín", "Castro Madrid"));
+        listaAlumnos.add(new Alumnos("Melina", "Victoria Godoy"));
+        listaAlumnos.add(new Alumnos("Román", "Ábalos Ishida"));
+        listaAlumnos.add(new Alumnos("Emilia", "Flores Luna"));
+
+        System.out.println("OficinaDeAlumnos: se cargaron " + listaAlumnos.size() + " alumnos.\n");
+    }
+}
--- guiaEjercicios2/src/main/java/ejercicio6/main.java
+++ guiaEjercicios2/src/main/java/ejercicio6/main.java
@@ -1,13 +1,29 @@
 package ejercicio6;
 
 public class main {
+    public static void main(String[] args) throws InterruptedException {
 
-	public static void main(String[] args){
-		Thread proceso = new Thread(new OficinaDeAlumnos("Benjamín","Castro Madrid"));
-		Thread proceso2 = new Thread(new OficinaDeAlumnos("Melina","Castro Madrid"));
-		proceso.start();
-		proceso2.start();
-		
-	}
+        Thread oficina = new Thread(new OficinaDeAlumnos());
+        oficina.start();
+        oficina.join();
 
-}
+        Thread preceptor = new Thread(new Preceptor());
+        Thread docente = new Thread(new Docente());
+
+        preceptor.start();
+        docente.start();
+
+        preceptor.join();
+        docente.join();
+
+        Docente.calcularPromedios();
+
+        System.out.println("Resultado final:");
+        for (Alumnos a : OficinaDeAlumnos.listaAlumnos) {
+            String estado = a.isAlumnoRegular()
+                    ? " | REGULAR"
+                    : " | LIBRE (menos de 75% asistencia)";
+            System.out.println(a.getNombre() + " " + a.getApellido() + estado + " | Promedio: " + a.getPromedio());
+            }
+        }
+    }
