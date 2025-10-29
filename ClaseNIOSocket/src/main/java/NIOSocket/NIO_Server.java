ec2c4d60bf9ecbb2a9ee08383e52d57a0e655be9
Sevn <benjamin.castromadrid28et32@gmail.com>
2025-10-29 12:36:45 -0300

    Clase 2 de NIOSocket
    Continuamos trabajando con Gonzalo

--- ClaseNIOSocket/src/main/java/NIOSocket/NIO_Server.java
+++ ClaseNIOSocket/src/main/java/NIOSocket/NIO_Server.java
@@ -1,16 +1,22 @@
 package NIOSocket;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class NIO_Server {
 	// Códigos ANSI para colores en consola
@@ -26,7 +32,8 @@ public class NIO_Server {
 	private ServerSocketChannel serverChannel;
 	
 	private boolean running = false;	
-	private final Gson json = new Gson();
+	private final Gson gson = new Gson();
+	PrintStream ps = new PrintStream(System.out);
 	
 	private Map<SocketChannel, String> usuarios = new HashMap<>();
 	private Map<String, SocketChannel> canalesPorNombre = new HashMap<>();
@@ -60,7 +67,57 @@ public class NIO_Server {
 								client.register(selector, SelectionKey.OP_READ);
 								System.out.print("Cliente conectado." + client.getRemoteAddress());
 								sendMessage(client);
-							}
+								JsonObject json = new JsonObject();
+								json.addProperty("type", "info");
+								json.addProperty("text", "Bienvenido!! Registrese enviando su nombre de usuario, gilipollas: ");
+								gson.toJson(json);
+								ByteBuffer bb = ByteBuffer.wrap(gson.toJson(json).getBytes());
+								int ByteLeidos = 0;
+								try {
+									ByteLeidos = client.read(bb);
+									client.write(bb);
+								}catch(IOException e) {
+									ps.println(ROJO + "[ERROR] no se puede enviar mensaje" + RESET);
+									disconnectClient(key, client);
+									return;
+								}
+								if(ByteLeidos == -1) {
+									disconnectClient(key, client);
+									return;
+								}
+								
+								bb.flip();
+								String msg = new String(bb.array(), 0, bb.limit()).trim();
+								
+								ps.println(AMARILLO + 
+										"[INFO] Mensaje recibido del cliente " + 
+										client.getLocalAddress().toString()+ 
+										":" + RESET);
+								try {
+									JsonObject json2 = JsonParser.parseString(msg).getAsJsonObject();
+									String tipo = json2.get("type").getAsString();
+									
+									if("command".equals(msg)) {
+										String comando = json2.get("command").getAsString().trim().toLowerCase();
+										handlerCommand(comando, client);
+										return;
+									}
+									
+									switch(tipo) {
+									case "register" ->{
+										
+										}
+									case "message" ->{
+										
+										}
+									
+									case "private" ->{}
+									
+									default ->{}
+									}
+									
+								}catch(Exception ex) {}
+							}							
 						}else if(key.isReadable()) {
 							readFromClient(key);
 						}
@@ -75,6 +132,48 @@ public class NIO_Server {
 						System.out.print("No rompas los huevos, servidor apagado.");
 					}
 			}).start();
+	}
+	
+	private void  readFromClient(SelectionKey key) {
+		SocketChannel client = (SocketChannel) key.channel();
+		ByteBuffer rb = ByteBuffer.allocate(2048);
+		try {
+			client.read(rb);
+		}catch(IOException e) {
+			ps.println(ROJO + "[ERROR] No se pudo leer el mensaje" + RESET);
+			
+		}
+	}
+	
+	private void handlerCommand(String cmd, SocketChannel cli) {
+		switch(cmd) {
+		case "/listUsers" ->{
+			//Mandar mensaje a todos los users
+			Set<String> listadoUsers = new HashSet<>(usuarios.values());
+			
+			JsonObject json = new JsonObject();
+			json.addProperty("type", "cmd_response");
+			json.addProperty("cmd", "/listUsers");
+			json.addProperty("text", "Usuarios conectados: " + String.join(",\n" , listadoUsers));
+			sendToClient(gson.toJson(json), cli);
+			ps.println(AZUL + "[CMD] Comando /listUsers solicitado por " + cli.getRemoteAddress().toString());
+			}
+		
+		case "/logout" ->{
+			//Desconectando al cliente
+			sendInfo(cli, "Desconectando...");
+			ps.println(AMARILLO + "[INFO] Desconectando al cliente" +
+			cli.getRemoteAddress().toString()
+			+ RESET);
+			disconnectClient(cli.keyFor(selector), cli);
+			}
+		
+		default ->{
+			sendError(cli, "Comando desconocido..." + cmd);
+			ps.println(AMARILLO + "[INFO] Desconectando al cliente" +
+			cmd + RESET);
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws IOException {
